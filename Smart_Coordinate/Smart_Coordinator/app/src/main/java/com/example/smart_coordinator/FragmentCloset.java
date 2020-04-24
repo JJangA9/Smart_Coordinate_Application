ae9f9e6c6aff7b08c5d3f9f8da96aedd2217698b
Jeong You Ji <dbwl150236@naver.com>
2020-04-24 16:28:13 +0900

    Camera
    camera function

--- Smart_Coordinate/Smart_Coordinator/app/src/main/java/com/example/smart_coordinator/FragmentCloset.java
+++ Smart_Coordinate/Smart_Coordinator/app/src/main/java/com/example/smart_coordinator/FragmentCloset.java
@@ -1,17 +1,51 @@
 package com.example.smart_coordinator;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class FragmentCloset extends Fragment{
+import java.io.File;
+
+public class FragmentCloset extends Fragment implements View.OnClickListener{
+    private ImageView imageView;
+    private Button button;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_closet, container, false);
+        View view = null;
+        view = inflater.inflate(R.layout.fragment_closet, container, false);
+
+        button = (Button)view.findViewById(R.id.cameraButton);
+        imageView = (ImageView)view.findViewById(R.id.capture);
+        button.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, 0);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        imageView.setImageURI(data.getData());
     }
 }
-
