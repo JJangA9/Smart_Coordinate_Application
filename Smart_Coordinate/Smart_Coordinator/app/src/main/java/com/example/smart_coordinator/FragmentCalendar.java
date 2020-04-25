89a98ea5fe303beea79a327224cb34d50c4379d7
Jeong You Ji <dbwl150236@naver.com>
2020-04-25 21:36:24 +0900

    Make material CalendarView

--- Smart_Coordinate/Smart_Coordinator/app/src/main/java/com/example/smart_coordinator/FragmentCalendar.java
+++ Smart_Coordinate/Smart_Coordinator/app/src/main/java/com/example/smart_coordinator/FragmentCalendar.java
@@ -1,16 +1,131 @@
 package com.example.smart_coordinator;
 
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.Executors;
 
 public class FragmentCalendar extends Fragment {
+    private Activity mContext;
+    String time, kcal, menu;
+    private final OneDayDecorator oneDayDecorator = new OneDayDecorator();
+    Cursor cursor;
+    MaterialCalendarView materialCalendarView;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.mContext = (Activity)context;
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = null;
+        view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        materialCalendarView = (MaterialCalendarView)view.findViewById(R.id.calendarView);
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(2017, 0, 1)) // start of calendar
+                .setMaximumDate(CalendarDay.from(2030, 11, 31))// end of calendar
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        materialCalendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator(),
+                oneDayDecorator);
+
+        String[] result = {"2020,04,12", "2020,04,18", "2020,05,18", "2020,06,18"};
+
+        new ApiSimulator(result).executeOnExecutor(Executors.newSingleThreadExecutor());
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                int Year = date.getYear();
+                int Month = date.getMonth() + 1;
+                int Day = date.getDay();
+
+                Log.i("Year test", Year + "");
+                Log.i("Month test", Month + "");
+                Log.i("Day test", Day + "");
+
+                String shot_Day = Year + "," + Month + "," + Day;
+
+                Log.i("shot_Day test", shot_Day + "");
+                materialCalendarView.clearSelection();
+                //Toast.makeText(getActivity().getApplicationContext(), shot_Day, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+    }
+
+    private class ApiSimulator extends AsyncTask<Void, Void, List<CalendarDay>> {
+        String[] Time_Result;
+
+        ApiSimulator(String[] Time_Result) {
+            this.Time_Result = Time_Result;
+        }
+
+        @Override
+        protected List<CalendarDay> doInBackground(@NonNull Void... voids) {
+            try {
+                Thread.sleep(500);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Calendar calendar = Calendar.getInstance();
+            ArrayList<CalendarDay> dates = new ArrayList<>();
+
+            /*Dot on the calendar for specific date*/
+            //get Time_Result and cut with ',' and convert string to int
+            for(int i = 0; i < Time_Result.length; i++) {
+                CalendarDay day = CalendarDay.from(calendar);
+                String[] time = Time_Result[i].split(",");
+                int year = Integer.parseInt(time[0]);
+                int month = Integer.parseInt(time[1]);
+                int dayy = Integer.parseInt(time[2]);
+
+                dates.add(day);
+                calendar.set(year, month-1, dayy);
+            }
+
+            return dates;
+        }
+
+        @Override
+        protected void onPostExecute(@NonNull List<CalendarDay> calendarDays) {
+            super.onPostExecute(calendarDays);
+
+            if(mContext.isFinishing()) {
+                return;
+            }
+
+            materialCalendarView.addDecorator(new EventDecorator(Color.GREEN, calendarDays, mContext));
+        }
     }
 }
