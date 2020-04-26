66dba594b098ca3cfe7333a34e8b54565871cd0d
Jeong You Ji <dbwl150236@naver.com>
2020-04-27 00:28:57 +0900

    GetImage&TakePicture
    get image from album and take picture

--- Smart_Coordinate/Smart_Coordinator/app/src/main/java/com/example/smart_coordinator/FragmentCloset.java
+++ Smart_Coordinate/Smart_Coordinator/app/src/main/java/com/example/smart_coordinator/FragmentCloset.java
@@ -1,27 +1,56 @@
 package com.example.smart_coordinator;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class FragmentCloset extends Fragment implements View.OnClickListener{
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
+public class FragmentCloset extends Fragment{
+    private Activity mContext;
+    private Context con;
+
+    private String imageFilePath;
+    private Uri photoUri;
+
+    private static final int PICK_FROM_CAMERA = 0;
+    private static final int PICK_FROM_ALBUM = 1;
+
     private ImageView imageView;
-    private Button button;
+    private Button cameraBtn;
+    private Button albumBtn;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.mContext = (Activity) context;
+        con = (Context) mContext;
+    }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,20 +61,101 @@ public class FragmentCloset extends Fragment implements View.OnClickListener{
         View view = null;
         view = inflater.inflate(R.layout.fragment_closet, container, false);
 
-        button = (Button)view.findViewById(R.id.cameraButton);
+        cameraBtn = (Button)view.findViewById(R.id.cameraButton);
+        albumBtn = (Button)view.findViewById(R.id.albumButton);
         imageView = (ImageView)view.findViewById(R.id.capture);
-        button.setOnClickListener(this);
+
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                if(cameraIntent.resolveActivity(mContext.getPackageManager()) != null) {
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        //Error handling
+                    }
+                    if (photoFile != null) {
+                        photoUri = FileProvider.getUriForFile(con, mContext.getPackageName(), photoFile);
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                        startActivityForResult(cameraIntent, PICK_FROM_CAMERA);
+                    }
+                }
+            }
+        });
+
+        albumBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, PICK_FROM_ALBUM);
+            }
+        });
+
         return view;
     }
 
     @Override
-    public void onClick(View view) {
-        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(i, 0);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PICK_FROM_ALBUM && resultCode==RESULT_OK&&data!=null&&data.getData()!=null) {
+            Uri selectedImageUri = data.getData();
+            imageView.setImageURI(selectedImageUri);
+            //imageView.setImageURI(data.getData());
+        } else if(requestCode == PICK_FROM_CAMERA && resultCode==RESULT_OK) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imageFilePath);
+            ExifInterface exif = null;
+
+            try {
+                exif = new ExifInterface(imageFilePath);
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+
+            int exifOrientation;
+            int exifDegree;
+
+            if(exif != null) {
+                exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                exifDegree = exifOrientationToDegrees(exifOrientation);
+            } else {
+                exifDegree = 0;
+            }
+
+            if(bitmap != null) {
+                imageView.setImageBitmap(rotate(bitmap, exifDegree));
+            }
+        }
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        imageView.setImageURI(data.getData());
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "TEST_" + timeStamp + "_";
+        File storageDir = mContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        imageFilePath = image.getAbsolutePath();
+        return image;
     }
+
+    private int exifOrientationToDegrees(int exifOrientation) {
+        if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+
+    //Rotate photo
+    private Bitmap rotate(Bitmap bitmap, float degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
 }
