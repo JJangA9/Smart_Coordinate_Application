6ec2a65d3b7342d460f350ae5adf0f9c4a6c2346
Jeong You Ji <dbwl150236@naver.com>
2020-05-22 02:40:44 +0900

    Add gif at start activity
    gif animation is added

--- Smart_Coordinate/Smart_Coordinator/app/src/main/java/com/example/smart_coordinator/StartActivity.java
+++ Smart_Coordinate/Smart_Coordinator/app/src/main/java/com/example/smart_coordinator/StartActivity.java
@@ -2,22 +2,37 @@ package com.example.smart_coordinator;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 public class StartActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        try {
-            Thread.sleep(3000);
-            startActivity(new Intent(this, MainActivity.class));
-            finish();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
         setContentView(R.layout.activity_start);
+
+        ImageView gifDot = (ImageView)findViewById(R.id.gif_image);
+        GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(gifDot);
+        Glide.with(this).load(R.raw.dotgif).into(gifImage);
+
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
     }
 }
