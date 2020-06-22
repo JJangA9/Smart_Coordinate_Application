ea162cb2988edccbfc5d1a4969dff6caf4f1525c
Jeong You Ji <dbwl150236@naver.com>
2020-06-23 00:17:16 +0900

    Modify closet design

--- Smart_Coordinate/Smart_Coordinator/app/src/main/java/com/example/smart_coordinator/FragmentCamera.java
+++ Smart_Coordinate/Smart_Coordinator/app/src/main/java/com/example/smart_coordinator/FragmentCamera.java
@@ -1,17 +1,28 @@
 package com.example.smart_coordinator;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
 
 public class FragmentCamera extends Fragment{
 
